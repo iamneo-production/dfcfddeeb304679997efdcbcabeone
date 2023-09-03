c8940e7a10ce7f5ae543c88cd8c81f0966f8fc54
Iamneo <techadmin@iamneo.ai>
2023-09-03 16:40:22 +0000

    Backup Commit

--- tic.js
+++ tic.js
@@ -3,5 +3,15 @@ let board=['','','','','','','','',''];
 let won=false;
 function makeMove(button){
     const index=parseInt(button.id);
-    if(!board[index] && )
+    if(!board[index] && !won){
+        board[index]=curr;
+        button.innerText=curr;
+        if(checkWin()){
+            document.getElementById('result').innerText='player ${curr} wins!'
+            won=true;
+        }else if(!board.includes('')){
+            document.getElementById('result').inner
+
+        }
+    }
 }
