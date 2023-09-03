ca6615b861619b535db1a3e2830294b6f4cfad31
Iamneo <techadmin@iamneo.ai>
2023-09-03 16:50:22 +0000

    Backup Commit

--- tic.js
+++ tic.js
@@ -7,14 +7,22 @@ function makeMove(button){
         board[index]=curr;
         button.innerText=curr;
         if(checkWin()){
-            document.getElementById('result').innerText='player ${curr} wins!'
+            document.getElementById('result').innerText=`player ${curr} wins!`
             won=true;
         }else if(!board.includes('')){
             document.getElementById('result').innerText="It's a draw!";
             won=true;
         }else{
             curr=(curr === 'X') ? '0' :'X';
-            document.getElementById('result').innerText='player ${curr}'s Turn'
+            document.getElementById('result').innerText=`player ${curr}'s Turn`;
         }
     }
+}
+function checkWin(){
+    const winConditions=[
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ];
+    for9const condition of winCo
 }
